22c7b307b21836ded52752db0fa4b02bc6fca089
harshitjail <harshitgwl@gmail.com>
2021-03-02 14:23:28 +0530

    done 2

--- program.java
+++ program.java
@@ -1,2 +1,15 @@
-second testing
+
+
+
+
+public class AddTwoNumbers {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
 
