dab07f139e2cf22ef5c118f9b55e99ef1ada0cbd
harshitjail <74790440+harshitjail@users.noreply.github.com>
2021-10-26 18:37:45 +0530

    jenkins

--- Hello.java
+++ Hello.java
@@ -1,2 +1,10 @@
-write code here
+public class AddTwoNumbers {
 
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+      System.out.println("JENKINS TESTING")
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
